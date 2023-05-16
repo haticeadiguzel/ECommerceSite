1b918daebe35d2cebfe1ec069239c7592706ffc4
Hatice Adıgüzel <77553404+haticeadiguzel@users.noreply.github.com>
2023-05-16 18:16:37 +0300

    2023-05-16 18:15

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
